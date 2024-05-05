32fc7ed6baf2e9afd245fd80a4afb2aa394971e5
Artem_Limonov <liemartt@yandex.ru>
2024-05-05 22:24:49 +0300

    feat: add mapstruct

--- src/main/java/com/liemartt/taxigarage/dao/repository/CarRepository.java
+++ src/main/java/com/liemartt/taxigarage/dao/repository/CarRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
+    Car findById(Long id);
+
+    void deleteById(Long id);
 }
