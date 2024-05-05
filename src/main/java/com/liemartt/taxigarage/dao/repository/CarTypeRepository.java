1dacb84fc6b864215b1d911a455c8da9d60ef071
Artem_Limonov <liemartt@yandex.ru>
2024-05-05 22:06:54 +0300

    feat: add @Repository annotation

--- src/main/java/com/liemartt/taxigarage/dao/repository/CarTypeRepository.java
+++ src/main/java/com/liemartt/taxigarage/dao/repository/CarTypeRepository.java
@@ -2,6 +2,8 @@ package com.liemartt.taxigarage.dao.repository;
 
 import com.liemartt.taxigarage.dao.entity.CarType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarTypeRepository extends JpaRepository<CarType, Long> {
 }
--- src/main/java/com/liemartt/taxigarage/dao/repository/UserRepository.java
+++ src/main/java/com/liemartt/taxigarage/dao/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.liemartt.taxigarage.dao.repository;
 
 import com.liemartt.taxigarage.dao.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
